30d9256fd6afb4b646a9c5254e18b56a949b681f
Phanindra <88643808+phanindra1033@users.noreply.github.com>
2024-02-01 20:03:39 +0530

    Delete Sa.py

--- Sa.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('G:/New folder (4)/csv1.csv','r') as csvinput: 
-    with open('G:/New folder (4)/tsv1.tsv', 'w') as tsvoutput:
-        csvinput = csv.reader(csvinput)
-        tsvoutput = csv.writer(tsvoutput, delimiter='\t')
-
-        for row in csvinput:	    
-            tsvoutput.writerow(row)
